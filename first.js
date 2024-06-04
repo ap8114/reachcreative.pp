d0b3dac687755a31fab07f447f1c31a0fbcff1b3
ap8114 <amanpatidar086@gmail.com>
2024-06-04 16:39:26 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,33 @@
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// Get the button and the card container
+let scrollRightBtn = document.getElementById("scrollRightBtn");
+let cardContainer = document.querySelector(".card-container");
+
+// When the user clicks on the button, scroll the card container to the right
+scrollRightBtn.onclick = function() {
+  cardContainer.scrollBy({
+    top: 0, // No vertical scroll
+    left: 200, // Adjust this value based on your card width
+    behavior: 'smooth' // Smooth scrolling
+  });
+}
